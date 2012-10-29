a25283af04b27b75f8c23a71fd79086d419608a9
sclarson <seth.larson@gmail.com>
2012-10-29 14:57:44 -0500

    Updates to make it easier to call from scripts

--- Program.cs
+++ Program.cs
@@ -1,34 +1,84 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Microsoft.Web.Publishing.Tasks;
 
 namespace TransformTesterConsole
 {
+    public class TransformArgs
+    {
+        public string SourceFile { get; set; }
+        public string TransformFile { get; set; }
+        public string DestinationFile { get; set; }
+
+        public TransformArgs() { }
+
+        public TransformArgs(string[] args)
+        {
+            this.SourceFile = args[0];
+            this.TransformFile = args[1];
+            if (args.Length > 2)
+            {
+                DestinationFile = args[2];
+            }
+            else
+            {
+                DestinationFile = args[0];
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            TransformArgs strongArgs = new TransformArgs();
+            if (args.Any(p=> p.Contains("help")))
+            {
+                Console.WriteLine("Usage: transform.exe <original file>  <tranformation file> [output file]");
+            }
+            else if (args.Length > 1)
+            {
+                strongArgs = new TransformArgs(args);
+            }
+            else
+            {
+                Console.Write("Enter the name of original configuration file:");
+                strongArgs.SourceFile = Path.Combine(Directory.GetCurrentDirectory(), Console.ReadLine());
+
+                Console.Write("Enter the name of the transformation file:");
+                strongArgs.TransformFile = Path.Combine(Directory.GetCurrentDirectory(), Console.ReadLine());
+
+                Console.Write("Enter the name of the output file:");
+                strongArgs.DestinationFile = Path.Combine(Directory.GetCurrentDirectory(), Console.ReadLine());
+            }
+
+            var successful = ProcessTransform(strongArgs);
+            Console.WriteLine("Transformer returned {0}",successful);
+
+            Environment.ExitCode = successful ? 0 : 1;
+        }
+
+        private static bool ProcessTransform(TransformArgs args)
+        {
+            if (string.Equals(args.SourceFile, args.DestinationFile))
+            {
+                var tempFileName = Path.GetTempFileName();
+                File.Delete(tempFileName);
+                File.Copy(args.SourceFile, tempFileName);
+                args.SourceFile = tempFileName;
+            }
+
             var engine = new TransformOnlyBuildEngine();
-            
-            Console.Write("Enter the name of original configuration file:");
-            var originalFile = Path.Combine(Directory.GetCurrentDirectory(), Console.ReadLine());
-
-            Console.Write("Enter the name of the transformation file:");
-            var transformationFile = Path.Combine(Directory.GetCurrentDirectory(), Console.ReadLine());
-
-            Console.Write("Enter the name of the output file:");
-            var outputFile =Path.Combine(Directory.GetCurrentDirectory(), Console.ReadLine());
 
             var transformer = new TransformXml
-                                  {
-                                      BuildEngine = engine,
-                                      Source = originalFile,
-                                      Destination = outputFile,
-                                      Transform = transformationFile
-                                  };
+            {
+                BuildEngine = engine,
+                Source = args.SourceFile,
+                Destination = args.DestinationFile,
+                Transform = args.TransformFile
+            };
 
-            bool execute = transformer.Execute();
-            Console.WriteLine("Transformer returned {0}",execute);
+            return transformer.Execute();
         }
     }
 }
